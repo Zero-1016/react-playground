fbfe5173abfdd1369688e4bbe38c321cab9a53d0
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-02-22 16:16:56 +0900

    AppMentors

--- school/src/chaptor_0/AppMentors.jsx
+++ school/src/chaptor_0/AppMentors.jsx
@@ -1,5 +1,36 @@
 import React from "react";
 
 export default function AppMentors() {
-  return <div></div>;
+  const User = {
+    name: "지형",
+    age: 23,
+    gender: "male",
+    favorite: [
+      {
+        age: 13,
+        food: "라면",
+      },
+      {
+        age: 23,
+        food: "육회",
+      },
+    ],
+  };
+  return (
+    <>
+      <div>
+        <div>
+          <h1>{user.name}</h1>
+          <p>{user.age}</p>
+          <ul>{user.gender}</ul>
+          {User.favorite.map((favorite, index) => {
+            <li key={index}>
+              {favorite.age} {favorite.food}
+            </li>;
+          })}
+          <button>멘토의 이름을 바꾸기</button>
+        </div>
+      </div>
+    </>
+  );
 }
--- school/src/index.js
+++ school/src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import AppChampionStore from "./chaptor_0/AppChampionStore";
+import AppMentors from "./chaptor_0/AppMentors";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppChampionStore />
+    <AppMentors />
   </React.StrictMode>
 );
 
