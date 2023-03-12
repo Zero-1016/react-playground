6c82df96a79cf8c5fc6f606bf97bd492cc984136
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-03-13 01:30:54 +0900

    chaptor 2 : 엘리먼트 렌더링

--- school/src/index.js
+++ school/src/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Clock from "./chaptor_4/Clock";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      <Clock></Clock>
+    </React.StrictMode>
+  );
+}, 1000);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
