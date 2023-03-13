00d2206d6178f8ecee869665134f0c113370a2a2
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-03-14 01:19:46 +0900

    chaptor 3: 컴포넌트와 props

--- school/src/index.js
+++ school/src/index.js
@@ -3,16 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Clock from "./chaptor_4/Clock";
+import CommentList from "./chaptor_5/CommentList";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-setInterval(() => {
-  root.render(
-    <React.StrictMode>
-      <Clock></Clock>
-    </React.StrictMode>
-  );
-}, 1000);
+root.render(
+  <React.StrictMode>
+    <CommentList />
+  </React.StrictMode>
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
