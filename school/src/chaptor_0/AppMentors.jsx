08082926cd6594e4fc3915efd55deb517948e83e
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-03-26 22:09:45 +0900

    modify react practice

--- school/src/chaptor_0/AppMentors.jsx
+++ school/src/chaptor_0/AppMentors.jsx
@@ -1,21 +1,39 @@
 import React, { useState } from "react";
 
 export default function AppMentors() {
-  const [user, setUser] = useState({
-    name: "지형",
-    age: 23,
-    favorites: [
-      {
-        class: "초등학생",
-        food: "라면",
-      },
-      {
-        class: "중학생",
-        food: "육회",
-      },
-    ],
-  });
+  const [user, setUser] = useState(input_data);
 
+  const addAge = () => {
+    const age = prompt("추억할 시절을 알려주세요");
+    const food = prompt("좋아했던 음식을 알려주세요");
+    setUser((prev) => ({
+      ...prev,
+      favorites: [...prev.favorites, { class: age, food }],
+    }));
+  };
+  const modifyFood = () => {
+    const age = prompt("추억할 시절을 알려주세요");
+    const new_food = prompt("좋아했던 음식을 알려주세요");
+    setUser((prev) => ({
+      ...prev,
+      favorites: [
+        ...prev.favorites.map((favorite) => {
+          return favorite.class === age ? { ...favorite, food: new_food } : favorite;
+        }),
+      ],
+    }));
+  };
+  const deleteAge = () => {
+    const age = prompt("추억할 시절을 알려주세요");
+    setUser((prev) => ({
+      ...prev,
+      favorites: [
+        ...prev.favorites.filter((favorite) => {
+          return favorite.class !== age;
+        }),
+      ],
+    }));
+  };
   return (
     <>
       <div>
@@ -30,51 +48,26 @@ export default function AppMentors() {
               </li>
             ))}
           </ul>
-          <button
-            onClick={() => {
-              const age = prompt("추억할 시절을 알려주세요");
-              const food = prompt("좋아했던 음식을 알려주세요");
-              setUser((prev) => ({
-                ...prev,
-                favorites: [...prev.favorites, { class: age, food }],
-              }));
-            }}
-          >
-            나이별 음식 추가하기
-          </button>
-          <button
-            onClick={() => {
-              const age = prompt("추억할 시절을 알려주세요");
-              const new_food = prompt("좋아했던 음식을 알려주세요");
-              setUser((prev) => ({
-                ...prev,
-                favorites: [
-                  ...prev.favorites.map((favorite) => {
-                    return favorite.class === age ? { ...favorite, food: new_food } : favorite;
-                  }),
-                ],
-              }));
-            }}
-          >
-            나이별 음식 수정하기
-          </button>
-          <button
-            onClick={() => {
-              const age = prompt("추억할 시절을 알려주세요");
-              setUser((prev) => ({
-                ...prev,
-                favorites: [
-                  ...prev.favorites.filter((favorite) => {
-                    return favorite.class !== age;
-                  }),
-                ],
-              }));
-            }}
-          >
-            나이별 음식 삭제하기
-          </button>
+          <button onClick={addAge}>나이별 음식 추가하기</button>
+          <button onClick={modifyFood}>나이별 음식 수정하기</button>
+          <button onClick={deleteAge}>나이별 음식 삭제하기</button>
         </div>
       </div>
     </>
   );
 }
+
+const input_data = {
+  name: "지형",
+  age: 23,
+  favorites: [
+    {
+      class: "초등학생",
+      food: "라면",
+    },
+    {
+      class: "중학생",
+      food: "육회",
+    },
+  ],
+};
