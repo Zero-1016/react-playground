f9fe6ac5c4c359b78625e2b88e38435c56d2df68
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-05-01 14:01:42 +0900

    react query

---
+++ query/src/components/MainProducts.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import Products from './Products';
+import { useQueryClient } from '@tanstack/react-query';
+
+export default function MainProducts() {
+  const [showLeftProducts, setShowLeftProducts] = useState(true);
+  const [showRightProducts, setShowRightProducts] = useState(true);
+  const client = useQueryClient();
+  return (
+    <main className='container'>
+      <div>
+        {showLeftProducts && <Products />}
+        <button onClick={() => setShowLeftProducts((show) => !show)}>
+          Toggle
+        </button>
+      </div>
+      <div>
+        {showRightProducts && <Products />}
+        <button onClick={() => setShowRightProducts((show) => !show)}>
+          Toggle
+        </button>
+      </div>
+      <button onClick={()=>{client.invalidateQueries('products', false)}}>정보가 업데이트 되었음</button>
+    </main>
+  );
+}
